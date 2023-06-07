e76555944fd6ae8369782c2a24fb858c14ea62f7
TahmimShovon <tahmim14@gmail.com>
2023-06-08 03:18:20 +0600

    Added PostDetails

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,8 +1,22 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, START_LOADING, END_LOADING, CREATE, DELETE, UPDATE } from '../constants/actionTypes'
+import { FETCH_ALL, FETCH_BY_SEARCH, FETCH_POST, START_LOADING, END_LOADING, CREATE, DELETE, UPDATE } from '../constants/actionTypes'
 import * as api from '../api';
 
 
 //Action Creators
+
+export const getPost = (id) => async(dispatch) =>{
+    try {
+        dispatch({ type: START_LOADING });
+        const { data } = await api.fetchPost(id);
+
+        dispatch({type: FETCH_POST, payload:data });
+        dispatch({ type: END_LOADING });
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+}
+
 export const getPosts = (page) => async(dispatch) =>{
     try {
         dispatch({ type: START_LOADING });
