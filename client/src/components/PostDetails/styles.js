37ff4a19002924ee87fef46868448a0e3f03ce16
TahmimShovon <tahmim14@gmail.com>
2023-06-21 04:19:23 +0600

    Uploaded client side on netlify

--- client/src/components/PostDetails/styles.js
+++ client/src/components/PostDetails/styles.js
@@ -6,14 +6,12 @@ export default makeStyles((theme) => ({
     objectFit: 'cover',
     width: '100%',
     maxHeight: '600px',
-
   },
   card: {
     display: 'flex',
     width: '100%',
     [theme.breakpoints.down('sm')]: {
-      flexWrap: 'wrap',
-      flexDirection: 'column',
+      flexDirection: 'column-reverse', // Swap the order of flex items on small screens
     },
   },
   section: {
@@ -25,6 +23,7 @@ export default makeStyles((theme) => ({
     marginLeft: '20px',
     [theme.breakpoints.down('sm')]: {
       marginLeft: 0,
+      marginTop: '20px', // Add top margin to the image section on small screens
     },
   },
   recommendedPosts: {
@@ -34,10 +33,30 @@ export default makeStyles((theme) => ({
     },
   },
   loadingPaper: {
-    display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', borderRadius: '15px', height: '39vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '20px',
+    borderRadius: '15px',
+    height: '39vh',
   },
   commentsOuterContainer: {
-    display: 'flex', justifyContent: 'space-between',
+    display: 'flex',
+    flexDirection: 'column-reverse', // Display comments in a column layout
+    marginTop: '20px', // Add top margin to separate comments from other content
+    [theme.breakpoints.up('md')]: {
+      flexDirection: 'row', // Revert to row layout on larger screens
+      marginTop: 0, // Remove top margin on larger screens
+    },
   },
-  commentsInnerContainer: { height: '200px', overflowY: 'auto', marginRight: '30px' },
+  commentsInnerContainer: {
+    flex: 1,
+    height: '200px',
+    overflowY: 'auto',
+    marginRight: '20px', // Add right margin to separate comments from other content
+    [theme.breakpoints.up('md')]: {
+      height: 'auto', // Allow comments container to grow dynamically on larger screens
+      marginRight: 0, // Remove right margin on larger screens
+    },
+  },
 }));
